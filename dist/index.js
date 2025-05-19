296bfa08f29aabcef88714dc1a829c2ea79d227a
maylvin <maylvin@hypecode.com>
2025-05-19 12:04:39 +0200

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.direBonjour = void 0;
 const direBonjour = (nom) => {
     return `Bonjour, ${nom} !`;
 };
-exports.default = direBonjour;
+exports.direBonjour = direBonjour;
