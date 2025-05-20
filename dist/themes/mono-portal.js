2d76bfdea830e537be6623ed3f95e3deda925049
maylvin <m.bargue@cliqdigital.com>
2025-05-20 13:12:03 +0200

    fix(sdk): test

--- dist/themes/mono-portal.js
+++ dist/themes/mono-portal.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = getMonoPortalTheme;
-function getMonoPortalTheme(themeName) {
+export default function getMonoPortalTheme(themeName) {
     return `je suis le thème ${themeName}`;
 }
