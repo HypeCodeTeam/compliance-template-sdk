a8b76f3be95212b088a943572ef67622957c6e89
maylvin <m.bargue@cliqdigital.com>
2025-05-20 12:44:37 +0200

    fix(sdk): test

--- index.ts
+++ index.ts
@@ -1,29 +1,3 @@
-import path from "path";
-import { pathToFileURL } from "url";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export async function useTheme(themeName: string): Promise<string> {
-  try {
-    const filePath = pathToFileURL(
-      path.join(__dirname, "theme", `${themeName}.js`)
-    ).href;
-
-    const themeModule = await import(filePath);
-
-    if (!themeModule.handler || typeof themeModule.handler !== "function") {
-      throw new Error(
-        `Module "${themeName}" n'exporte pas de fonction handler.`
-      );
-    }
-
-    return themeModule.handler();
-  } catch (err) {
-    throw new Error(
-      `Erreur lors du chargement du thème "${themeName}" : ${err}`
-    );
-  }
+export async function useTheme(themeName: string) {
+  return {themeName}
 }
